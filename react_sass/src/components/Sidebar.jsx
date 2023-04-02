211b2bd7a511f92c29bf310e6a3c18a3386dead3
Bruno-Vitor <brunovitorc@gmail.com>
2023-04-01 23:59:33 -0300

    Update 1.2

--- react_sass/src/components/Sidebar.jsx
+++ react_sass/src/components/Sidebar.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Avatar from '../img/bc.jpg';
+import '../styles/components/sidebar.sass';
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <aside id='sidebar'>
+      <p>imagem</p>
+      <p className='title'>Front-end Developer</p>
+      <p>redes sociais</p>
+      <p>Informações de contato</p>
+      <a href="" className='btn'>
+        Download currículo
+      </a>
+    </aside>
   )
 }
 
