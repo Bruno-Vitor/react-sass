db4fd5bef56a33e8c001276f567e892532aba48f
Bruno-Vitor <brunovitorc@gmail.com>
2023-04-04 15:57:25 -0300

    Update 1.4

--- react_sass/src/components/Sidebar.jsx
+++ react_sass/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import SocialNetworks from './SocialNetworks';
 import Avatar from '../img/bc.jpg';
 import '../styles/components/sidebar.sass';
 
@@ -7,7 +8,7 @@ const Sidebar = () => {
     <aside id='sidebar'>
       <img src={Avatar} alt="Bruno Corrêa" />
       <p className='title'>Front-end Developer</p>
-      <p>redes sociais</p>
+      <SocialNetworks />
       <p>Informações de contato</p>
       <a href="" className='btn'>
         Download currículo
