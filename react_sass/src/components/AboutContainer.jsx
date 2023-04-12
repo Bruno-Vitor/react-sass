4c15ae1551f1f706660e4c00aa1a4bfa52c24dba
Bruno-Vitor <brunovitorc@gmail.com>
2023-04-12 11:46:31 -0300

    Update 1.6

---
+++ react_sass/src/components/AboutContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AboutContainer = () => {
+  return (
+    <section className='about-container'>
+      <h2>Sobre</h2>
+      <p>Olá! Meu nome é Bruno e sou um desenvolvedor front-end júnior. Sinto grande entusiasmo pela tecnologia e adoro criar soluções criativas e funcionais que tornem a experiência do usuário mais agradável e intuitiva.
+      Tenho experiência em React.js, JavaScript e WordPress e estou sempre em busca de aprender novas tecnologias e ferramentas para me manter atualizado. Sempre estou disposto a compartilhar conhecimento, pois acredito que um ambiente colaborativo é essencial para o sucesso de um projeto. E para o desenvolvimento dos demais colaboradores.
+      </p>
+      <p>Acredito que a chave para ser um bom desenvolvedor é ter uma mentalidade de aprendizado contínuo e ser sempre proativo em relação aos desafios e oportunidades que surgem no dia a dia.</p>
+    </section>
+  );
+};
+
+export default AboutContainer
