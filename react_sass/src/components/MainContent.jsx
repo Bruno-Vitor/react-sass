f1c633928418030ce1d5735b7cfb3d0a6142183c
Bruno-Vitor <brunovitorc@gmail.com>
2023-04-19 12:23:18 -0300

    Update 1.8

--- react_sass/src/components/MainContent.jsx
+++ react_sass/src/components/MainContent.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import AboutContainer from './AboutContainer';
 import ProjectsContainer from './ProjectsContainer';
 import TechnologiesContainer from './TechnologiesContainer';
+
 import '../styles/components/maincontent.sass';
 
 const MainContent = () => {
